e3a6ebb4581d24c63929fe7b47f3a5600bbebae5
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2015-08-03 14:53:19 +0200

    Cleaning out unnecessary output.

--- src/command_line_parser_generator-run.adb
+++ src/command_line_parser_generator-run.adb
@@ -21,8 +21,9 @@ procedure Command_Line_Parser_Generator.Run is
    Compilation_Unit    : Asis.Compilation_Unit;
    Package_Declaration : Asis.Declaration;
 
-   Profiles    : Procedure_Declaration_List.Instance;
-   A_Procedure : Procedure_Declaration.Instance;
+   Package_Name : Source_Text;
+   Profiles     : Procedure_Declaration_List.Instance;
+   A_Procedure  : Procedure_Declaration.Instance;
 begin
    Setup (Context => Context);
 
@@ -57,15 +58,11 @@ begin
 
    Show_Package_Declaration :
    declare
-      use Ada.Wide_Text_IO;
-      use Asis.Declarations, Asis.Elements, Asis.Text;
+      use Asis.Elements;
    begin
       Package_Declaration := Unit_Declaration (Compilation_Unit);
 
-      Put      (Item => "package ");
-      Put      (Item => Defining_Name_Image (Names (Package_Declaration) (1)));
-      Put_Line (Item => " is");
-      New_Line;
+      Package_Name := +Utilities.Defining_Name (Package_Declaration);
    end Show_Package_Declaration;
 
    Visible_Declaration_Elements :
@@ -78,14 +75,8 @@ begin
    begin
       for Declaration of Declarations loop
          if Declaration_Kind (Declaration) = A_Procedure_Declaration then
-            Put_Line (Item => Element_Image (Declaration));
-            New_Line;
-            Put      (Item => "procedure ");
-            Put      (Item => Defining_Name_Image (Names (Declaration) (1)));
-
             A_Procedure :=
-              (Name              => +Utilities.Defining_Name
-                 (Declaration),
+              (Name              => +Utilities.Defining_Name (Declaration),
                Formal_Parameters => <>);
 
             Show_Parameters :
@@ -102,117 +93,83 @@ begin
                   Default_Value     => +"<default value>",
                   Type_Name         => +"<type name>");
             begin
-               if Parameters'Length = 0 then
-                  Put_Line (Item => ";");
-               else
-                  New_Line;
-                  Put_Line (Item => "  (");
+               for Parameter of Parameters loop
+                  for Name of Names (Parameter) loop
+                     A_Formal_Parameter.Name := +Defining_Name_Image (Name);
 
-                  for Parameter of Parameters loop
-                     for Name of Names (Parameter) loop
-                        Put (Item => "     ");
-                        Put (Item => Defining_Name_Image (Name));
-                        Put (Item => " : ");
+                     case Mode_Kind (Parameter) is
+                        when A_Default_In_Mode | An_In_Mode =>
+                           null; --  Fine, we continue.
+                        when An_In_Out_Mode | An_Out_Mode =>
+                           Put_Line (File => Standard_Error,
+                                     Item => "Out parameters not allowed.");
+                           Set_Exit_Status (Failure);
+                           return;
+                        when Not_A_Mode =>
+                           Put_Line (File => Standard_Error,
+                                     Item => "ASIS error.");
+                           Set_Exit_Status (Failure);
+                           return;
+                     end case;
 
-                        A_Formal_Parameter.Name := +Defining_Name_Image (Name);
+                     if Has_Aliased (Parameter) then
+                        Put_Line (File => Standard_Error,
+                                  Item => "Aliased parameters not allowed.");
+                        Set_Exit_Status (Failure);
+                        return;
+                     end if;
 
-                        case Mode_Kind (Parameter) is
-                           when A_Default_In_Mode | An_In_Mode =>
-                              Put (Item => "in     ");
-                           when An_In_Out_Mode =>
-                              Put (Item => "in out ");
-                              Put_Line
-                                (File => Standard_Error,
-                                 Item => "Out parameters not allowed.");
-                              Set_Exit_Status (Failure);
-                              return;
-                           when An_Out_Mode =>
-                              Put (Item => "   out ");
-                              Put_Line
-                                (File => Standard_Error,
-                                 Item => "Out parameters not allowed.");
-                              Set_Exit_Status (Failure);
-                              return;
-                           when Not_A_Mode =>
+                     Full_Type_Name :
+                     declare
+                        use Utilities;
+
+                        Type_Of_Parameter : constant Asis.Declaration :=
+                          Object_Declaration_View (Parameter);
+                     begin
+                        A_Formal_Parameter.Type_Name :=
+                          +Full_Defining_Name (Type_Of_Parameter);
+                     end Full_Type_Name;
+
+                     Check_For_Default_Value :
+                     declare
+                        use all type Asis.Element_Kinds;
+
+                        Value : constant Asis.Expression :=
+                                  Initialization_Expression (Parameter);
+                     begin
+                        case Element_Kind (Value) is
+                           when Not_An_Element =>
+                              null; --  No default value.
+
+                              A_Formal_Parameter.Default_Value := +"";
+                           when An_Expression =>
+                              A_Formal_Parameter.Default_Value :=
+                                +Trim (Element_Image (Value));
+                           when others =>
+                              Put_Line (File => Standard_Error,
+                                        Item => Debug_Image (Value));
                               Put_Line (File => Standard_Error,
                                         Item => "ASIS error.");
                               Set_Exit_Status (Failure);
                               return;
                         end case;
+                     end Check_For_Default_Value;
 
-                        if Has_Aliased (Parameter) then
-                           Put_Line (Item => "aliased");
-                           Put_Line
-                             (File => Standard_Error,
-                              Item => "Aliased parameters not allowed.");
-                           Set_Exit_Status (Failure);
-                           return;
-                        end if;
+                     Image_And_Value_Functions :
+                     declare
+                        use type Source_Text;
+                     begin
+                        A_Formal_Parameter.Image_Function :=
+                          A_Formal_Parameter.Type_Name & "'Image";
+                        A_Formal_Parameter.Value_Function :=
+                          A_Formal_Parameter.Type_Name & "'Value";
+                     end Image_And_Value_Functions;
 
-                        declare
-                           use Utilities;
-
-                           Type_Of_Parameter : constant Asis.Declaration :=
-                             Object_Declaration_View (Parameter);
-                        begin
-                           Put (Full_Defining_Name (Type_Of_Parameter));
-
-                           Generate_Reader (For_Type => Type_Of_Parameter);
-
-                           A_Formal_Parameter.Type_Name :=
-                             +Full_Defining_Name (Type_Of_Parameter);
-                        end;
-
-                        Check_For_Default_Value :
-                        declare
-                           use all type Asis.Element_Kinds;
-
-                           Value : constant Asis.Expression :=
-                                     Initialization_Expression (Parameter);
-                        begin
-                           case Element_Kind (Value) is
-                              when Not_An_Element =>
-                                 null; --  No default value.
-
-                                 A_Formal_Parameter.Default_Value := +"";
-                              when An_Expression =>
-                                 Put (Item => " := ");
-                                 Put (Item => Trim (Element_Image (Value)));
-
-                                 A_Formal_Parameter.Default_Value :=
-                                   +Trim (Element_Image (Value));
-                              when others =>
-                                 Put_Line (File => Standard_Error,
-                                           Item => Debug_Image (Value));
-                                 Put_Line (File => Standard_Error,
-                                           Item => "ASIS error.");
-                                 Set_Exit_Status (Failure);
-                                 return;
-                           end case;
-                        end Check_For_Default_Value;
-
-                        Put_Line (Item => ";");
-
-                        Image_And_Value_Functions :
-                        declare
-                           use type Source_Text;
-                        begin
-                           A_Formal_Parameter.Image_Function :=
-                             A_Formal_Parameter.Type_Name & "'Image";
-                           A_Formal_Parameter.Value_Function :=
-                             A_Formal_Parameter.Type_Name & "'Value";
-                        end Image_And_Value_Functions;
-
-                        A_Procedure.Formal_Parameters.Append
-                          (A_Formal_Parameter);
-                     end loop;
+                     A_Procedure.Formal_Parameters.Append
+                       (A_Formal_Parameter);
                   end loop;
-
-                  Put_Line (Item => "  );");
-               end if;
+               end loop;
             end Show_Parameters;
-
-            New_Line;
 
             Profiles.Append (A_Procedure);
          elsif Declaration_Kind (Declaration) = A_Function_Declaration then
@@ -226,13 +183,16 @@ begin
             Set_Exit_Status (Failure);
             return;
          else
-            Put_Line ("""" & Element_Image (Declaration) & """ is ignored.");
+            Put_Line (File => Standard_Error,
+                      Item => "Warning: """ & Element_Image (Declaration) &
+                              """ is ignored.");
          end if;
-
-         New_Line;
       end loop;
 
+      Put_Line ("package " & (+Package_Name) & " is");
+      New_Line;
       Put_Line (Profiles.Image);
+      Put_Line ("end " & (+Package_Name) & ";");
    exception
       when others =>
          Put_Line (Standard_Error,
