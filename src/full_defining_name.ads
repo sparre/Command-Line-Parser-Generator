64619a37eff8a4e493bef71cfc18297de116b8d2
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2015-08-03 10:12:25 +0200

    Simplifying 'Generate_Reader'.

---
+++ src/full_defining_name.ads
@@ -0,0 +1,3 @@
+with Asis;
+
+function Full_Defining_Name (Item : Asis.Declaration) return Wide_String;
--- src/trim.adb
+++
@@ -1,7 +0,0 @@
-with Ada.Strings.Wide_Fixed;
-
-function Trim (Item : in Wide_String) return Wide_String is
-   use Ada.Strings;
-begin
-   return Wide_Fixed.Trim (Item, Both);
-end Trim;
