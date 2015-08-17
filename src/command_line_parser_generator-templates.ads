4931e73a1a465e5f19c7ee2d4230c47f06414204
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2015-08-17 16:47:46 +0200

    Generate runner procedure.

---
+++ src/command_line_parser_generator-templates.ads
@@ -0,0 +1,14 @@
+private
+with Ada.Wide_Text_IO;
+
+package Command_Line_Parser_Generator.Templates is
+   procedure Set    (Target_Directory : in String);
+   procedure Create (Target_Directory : in String);
+
+   procedure Runner (Package_Name : in     Wide_String);
+private
+   procedure Create_Specification (Name : in     Wide_String;
+                                   File : in out Ada.Wide_Text_IO.File_Type);
+   procedure Create_Body (Name : in     Wide_String;
+                          File : in out Ada.Wide_Text_IO.File_Type);
+end Command_Line_Parser_Generator.Templates;
