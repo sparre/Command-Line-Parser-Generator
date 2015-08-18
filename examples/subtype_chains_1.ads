88159863cb304f75803564173632496978e9b028
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2015-08-18 13:03:39 +0200

    A package has to have at least one public procedure to make sense for the command line parser generator.

--- examples/subtype_chains_1.ads
+++ examples/subtype_chains_1.ads
@@ -1,4 +1,6 @@
 package Subtype_Chains_1 is
    subtype One is Integer
      with Dynamic_Predicate => One /= 2;
+
+   procedure Something_To_Call;
 end Subtype_Chains_1;
--- examples/subtype_chains_2.ads
+++ examples/subtype_chains_2.ads
@@ -3,4 +3,7 @@ with Subtype_Chains_1;
 package Subtype_Chains_2 is
    subtype Two is Subtype_Chains_1.One
      with Dynamic_Predicate => Two mod 2 = 0;
+
+   procedure Something_To_Call
+     renames Subtype_Chains_1.Something_To_Call;
 end Subtype_Chains_2;
