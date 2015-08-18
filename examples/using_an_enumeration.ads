ff1d9d46110efff795dfd0b06ee73c9aefb500b1
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2015-08-18 14:46:01 +0200

    Expanded an example.

--- examples/using_an_enumeration.ads
+++ examples/using_an_enumeration.ads
@@ -2,4 +2,7 @@ package Using_An_Enumeration is
    type Choices is (Interactive, Batch, Service);
 
    procedure Run (Mode : in Choices := Interactive);
+
+   procedure Run (Configuration_File : in     String;
+                  Mode               : in     Choices);
 end Using_An_Enumeration;
