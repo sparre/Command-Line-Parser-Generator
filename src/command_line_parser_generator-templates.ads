695600de06a11adcbe0bf62dc8edd0d1d6e03e80
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2015-08-19 08:51:49 +0200

    Templates: Factoring out a simple procedure call.  Checking if arguments are going to be ignored.

--- src/command_line_parser_generator-templates.ads
+++ src/command_line_parser_generator-templates.ads
@@ -3,6 +3,9 @@ with Command_Line_Parser_Generator.Procedure_Declaration_List;
 private
 with Ada.Wide_Text_IO;
 
+private
+with Command_Line_Parser_Generator.Procedure_Declaration;
+
 package Command_Line_Parser_Generator.Templates is
    procedure Set    (Target_Directory : in String);
    procedure Create (Target_Directory : in String);
@@ -20,4 +23,12 @@ private
                                    File : in out Ada.Wide_Text_IO.File_Type);
    procedure Create_Body (Name : in     Wide_String;
                           File : in out Ada.Wide_Text_IO.File_Type);
+
+   subtype Simple_Procedure is Procedure_Declaration.Instance
+     with Dynamic_Predicate
+            => Simple_Procedure.Number_Of_Optional_Parameters = 0;
+
+   procedure Call (Target      : in     Ada.Wide_Text_IO.File_Type;
+                   Profile     : in     Simple_Procedure;
+                   Indentation : in     Natural);
 end Command_Line_Parser_Generator.Templates;
