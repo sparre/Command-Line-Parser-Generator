e3f4e4b849eb2e5519a92d8d0f9ce13832fcf088
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2015-11-11 14:12:09 +0100

    Examples: Provide help texts (but no 'Show_Help' procedure).

--- examples/web_site_generator.ads
+++ examples/web_site_generator.ads
@@ -1,3 +1,11 @@
+private
+with Ada.Text_IO;
+
 package Web_Site_Generator is
-   procedure Run;
+   type Flag is (Present);
+
+   procedure Run (Run : in     Flag);
+   procedure Fail (Fail : in     Flag);
+private
+   procedure Put_Help (File : in     Ada.Text_IO.File_Type);
 end Web_Site_Generator;
